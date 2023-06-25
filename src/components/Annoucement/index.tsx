e47eb30e5c46ae625a4949d38873c9d3edb569f7
Gustavo Nep <gustavo._12@hotmail.com>
2023-06-25 17:16:16 -0300

    fix: styled bug compiler

---
+++ src/components/Annoucement/index.tsx
@@ -0,0 +1,20 @@
+"use client";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+export default function Annoucement() {
+  return (
+    <Container>
+      <div>
+        <button>WEBINARES EXCLUSIVOS</button>
+        <div>
+          <h3>Menos Conversinha,</h3>
+        </div>
+        <div>
+          <h1>Mais Conversão</h1>
+        </div>
+      </div>
+    </Container>
+  );
+}
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,11 @@
+import Annoucement from "@/components/Annoucement";
 import Navbar from "@/components/Navbar";
 
 export default function Landing() {
-    return(
-        <Navbar />
-    )
+  return (
+    <>
+      <Navbar />
+      <Annoucement />
+    </>
+  );
 }
